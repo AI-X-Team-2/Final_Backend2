7d0789f5e011f76f16e8c834ce704f706650de15
Seolhw02 <frg7994tui5@gmail.com>
2025-08-25 14:49:44 +0900

    깃 주소 변경 4

--- main.py
+++ main.py
@@ -22,10 +22,13 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="유음 database")
 
 origins = [
-    "http://3.35.0.140/",  # Vite dev server
-    "http://3.35.0.140/5173", # 필요하면 이것도 추가
-    "http://3.35.0.140/5174",  # 현재 프론트엔드 주소 추가!
-    "http://3.35.0.140", # 이것도 함께 추가
+    "http://localhost:5173",  # Vite dev server
+    "http://localhost:5174",  # 현재 프론트엔드 주소 추가
+  
+    "http://3.35.0.140/", 
+    "http://3.35.0.140/5173", 
+    "http://3.35.0.140/5174", 
+    "http://3.35.0.140", 
     
 ]
 
